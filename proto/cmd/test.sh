8a15ac0c7eab12ba6987f22be69cd902e4aee8ed
Michael Pankov <constantius9@gmail.com>
2014-10-29 17:05:57 +0300

    Add tests to prototype of command tracking

---
+++ proto/cmd/test.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+QAKE=../../qake
+
+set -ex
+
+set_up () {
+    export RELAY_DEBUG=
+}
+
+case_full_build () {
+    rm -rf build
+    $QAKE 2>&1 | sort > log
+    cat full_build.log | sort > full_build.log.sorted
+    diff -q log full_build.log.sorted
+}
+
+case_null_build () {
+    rm -rf build
+    $QAKE >/dev/null 2>&1
+    $QAKE | sort > log
+    cat null_build.log | sort > null_build.log.sorted
+    diff -q log null_build.log.sorted
+}
+
+case_meaningless_change_build () {
+    rm -rf build
+    $QAKE >/dev/null 2>&1
+    touch build/res/a
+    $QAKE | sort > log
+    cat meaningless_change_build.log | sort > meaningless_change_build.log.sorted
+    diff -q log meaningless_change_build.log.sorted
+}
+
+case_meaningful_change_build () {
+    rm -rf build
+    $QAKE >/dev/null 2>&1
+    echo aaaa > build/res/a
+    $QAKE | sort > log
+    cat meaningful_change_build.log | sort > meaningful_change_build.log.sorted
+    diff -q log meaningful_change_build.log.sorted
+}
+
+set_up
+case_full_build
+case_null_build
+case_meaningless_change_build
+case_meaningful_change_build
